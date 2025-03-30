695ed384cd1bdd2e93f47cce618b0844e506aef5
Eltayeb Ahmed <eltayeb.ahmed@eng.ox.ac.uk>
2025-03-30 15:30:21 +0100

    Data generation completion

--- motivation/data_utils.py
+++ motivation/data_utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import dataclasses
+import matplotlib.pyplot as plt
+import numpy as np
 import tyro
 
+### Data generation utilities.
 @dataclasses.dataclass
 class Distribution:
     """Distribution over a consecutive integer range."""
@@ -27,7 +30,7 @@ class DataPoint:
 def generate_data(
     n_operands: int, n_samples: int, distributions: list[Distribution],
     seed: int = 42
-) -> list[list[int]]:
+) -> list[DataPoint]:
     """Generate random data according to the given distributions."""
     assert len(distributions) == n_operands
 
@@ -75,7 +78,7 @@ def generate_dataset_with_skewed_operands(n_samples: int,
     min_operand: int,
     max_operand: int,
     alpha: float = 0.14,
-    seed: int = 42):
+    seed: int = 42) -> list[DataPoint]:
     """Generate a dataset with skewed operands."""
 
     assert n_operands > 1
@@ -89,13 +92,62 @@ def generate_dataset_with_skewed_operands(n_samples: int,
     
     return generate_data(n_operands, n_samples, distributions, seed)
 
-def main():
-    import matplotlib.pyplot as plt
-    import os
-    print(os.getcwd())
-    plt.plot(exponential_distribution(0, 10, -0.14).probabilities)
-    plt.savefig("exponential_distribution.png")
+def write_dataset_to_disk(data: list[DataPoint], output_filepath: str):
+    """Write the dataset to disk."""
+    assert output_filepath.endswith(".csv"), "Output path must end with .csv"
+    with open(output_filepath, "w") as f:
+        for data_point in data:
+            f.write(f"{' '.join(data_point.equation)}, {data_point.result}\n")
 
-def generate_data
-if __name__ == "__main__":
-    main()
+### Data visualization utilities.
+
+def visualize_operand_histogram(data: list[DataPoint], n_bins: int = 100):
+    """Visualize the per position histogram of operands in the dataset."""
+    assert data, "Empty dataset."
+    n_operands = len(data[0].equation) // 2 + 1
+
+    for data_point in data:
+        assert len(data_point.equation) == 2 * n_operands - 1
+    
+    fig, axs = plt.subplots(n_operands, 1, figsize=(10, 5 * n_operands))
+    for i in range(n_operands):
+        operands = [int(data_point.equation[2 * i]) for data_point in data]
+        axs[i].hist(operands, bins=n_bins)
+        axs[i].set_title(f"Operand {i + 1}")
+
+
+# ### Script entry point utils.
+
+# @dataclasses.dataclass
+# class GenerationConfig:
+#     """Configuration for data generation.
+    
+#     Will generate a skewed dataset with operands exponentially distributed 
+#         with alpha values ranging from -alpha to alpha.
+#     """
+#     n_samples: int
+#     n_operands: int
+#     min_operand: int
+#     max_operand: int
+#     alpha: float
+#     seed: int
+#     output_filepath: str
+
+# def main():
+
+#     config = tyro.cli(GenerationConfig)
+#     dataset = generate_dataset_with_skewed_operands(
+#         n_samples=config.n_samples, 
+#         n_operands=config.n_operands, 
+#         min_operand=config.min_operand, 
+#         max_operand=config.max_operand, 
+#         alpha=config.alpha, 
+#         seed=config.seed
+#     )
+#     write_dataset_to_disk(dataset, config.output_filepath)
+#     visualize_operand_histogram(dataset)
+#     plt.savefig(config.output_filepath.replace(".csv", ".png"))
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ motivation/generate_data.py
@@ -0,0 +1,49 @@
+import dataclasses
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import tyro
+
+# Guarantees that the module can be imported from the parent directory.
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from data_utils import (
+    generate_dataset_with_skewed_operands,
+    visualize_operand_histogram,
+    write_dataset_to_disk,
+)
+
+
+@dataclasses.dataclass
+class GenerationConfig:
+    """Configuration for data generation.
+
+    Will generate a skewed dataset with operands exponentially distributed
+        with alpha values ranging from -alpha to alpha.
+    """
+
+    n_samples: int
+    n_operands: int
+    min_operand: int
+    max_operand: int
+    alpha: float
+    seed: int
+    output_filepath: str
+
+
+
+if __name__ == "__main__":
+    config = tyro.cli(GenerationConfig)
+    dataset = generate_dataset_with_skewed_operands(
+        n_samples=config.n_samples,
+        n_operands=config.n_operands,
+        min_operand=config.min_operand,
+        max_operand=config.max_operand,
+        alpha=config.alpha,
+        seed=config.seed,
+    )
+    write_dataset_to_disk(dataset, config.output_filepath)
+    visualize_operand_histogram(dataset, n_bins=config.n_operands * 2)
+    plt.savefig(config.output_filepath.replace(".csv", ".png"))
+
